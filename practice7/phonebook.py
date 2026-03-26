6f35277665f70b0ed3f7296ea4b878f24cf6f978
Yernurkyzy Aknur <aknur.yernurkyzy@icloud.com>
2026-03-26 12:59:13 +0500

    Add PhoneBook project with PostgreSQL integration

--- practice7/phonebook.py
+++ practice7/phonebook.py
@@ -1,24 +1,53 @@
-#1
+import psycopg2
+import csv
+from config import params, CREATE_TABLE_COMMAND, CSV_FILE_PATH
+from connect import initialize_db
+
+# 1
 def insert_contact(name, phone):
-    sql = "INSERT INTO contacts(contact_name, phone_number) VALUES(%s, %s);"
+    sql = "INSERT INTO contacts(contact_name, phone_number) VALUES(%s, %s) ON CONFLICT DO NOTHING;"
     with psycopg2.connect(**params) as conn:
         with conn.cursor() as cur:
             cur.execute(sql, (name, phone))
-    print(f"Contact {name} added successfully!")
+    print(f"Contact {name} saved.")
 
-#2
-def insert_many_from_csv(file_path):
-    sql = "INSERT INTO contacts(contact_name, phone_number) VALUES(%s, %s);"
-    with open(file_path, 'r') as f:
+def upload_csv():
+    with open(CSV_FILE_PATH, 'r') as f:
         reader = csv.reader(f)
-        next(reader)  # тақырыбын аттап өту
-        data = [row for row in reader]
+        next(reader) 
+        for row in reader:
+            insert_contact(row[0], row[1])
+
+# 2
+def search_contact(query):
+    sql = "SELECT * FROM contacts WHERE contact_name ILIKE %s OR phone_number LIKE %s;"
     with psycopg2.connect(**params) as conn:
         with conn.cursor() as cur:
-            cur.executemany(sql, data) # Бірден бірнешеуін қосады
+            cur.execute(sql, (f"%{query}%", f"%{query}%"))
+            results = cur.fetchall()
+            for r in results:
+                print(f"ID: {r[0]} | Name: {r[1]} | Phone: {r[2]}")
 
-#3
-def insert_safe(name, phone):
-    sql = "INSERT INTO contacts(contact_name, phone_number) VALUES(%s, %s) ON CONFLICT (phone_number) DO NOTHING;"
+#3 
+def update_phone(name, new_phone):
+    sql = "UPDATE contacts SET phone_number = %s WHERE contact_name = %s;"
+    with psycopg2.connect(**params) as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql, (new_phone, name))
+            print(f"{name}'s phone updated.")
+
+def delete_contact(name):
+    sql = "DELETE FROM contacts WHERE contact_name = %s;"
+    with psycopg2.connect(**params) as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql, (name,))
+            print(f"Contact {name} deleted.")
+
+# MAIN PROGRAM
+if __name__ == "__main__":
+    initialize_db(CREATE_TABLE_COMMAND) 
     
-
+    upload_csv()
+    
+    print("\nSearch results for 'Ali':")
+    search_contact("Ali")
