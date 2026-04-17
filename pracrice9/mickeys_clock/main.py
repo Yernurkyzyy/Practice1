5d539f7e0cf0d346b96fa552652b58d0daf4b4c1
Yernurkyzy Aknur <aknur.yernurkyzy@icloud.com>
2026-04-17 21:16:33 +0500

    Final updates for Practice9: fixed music and folder structure

--- pracrice9/mickeys_clock/main.py
+++ pracrice9/mickeys_clock/main.py
@@ -1,13 +1,25 @@
 import pygame
+import pygame
 import datetime
+import os
 
 pygame.init()
 W, H = 800, 800
 screen = pygame.display.set_mode((W, H))
+pygame.display.set_caption("Mickey's Clock")
 
-# Суреттерді жүктеу (аттарын тексер!)
-bg = pygame.image.load('mickeys_clock/images/mainclock.png')
-hand = pygame.image.load('mickeys_clock/images/mickey_hand.png')
+
+try:
+
+    bg = pygame.image.load('images/mainclock.png')
+    hand = pygame.image.load('images/mickey_hand.png')
+except:
+    
+    print("Сурет табылмады! Тексеру режимі қосылды.")
+    bg = pygame.Surface((W, H))
+    bg.fill((255, 255, 255)) # Ақ фон
+    hand = pygame.Surface((400, 10), pygame.SRCALPHA)
+    pygame.draw.rect(hand, (0, 0, 0), (0, 0, 300, 10)) 
 
 def rotate(image, angle):
     rotated = pygame.transform.rotate(image, angle)
@@ -15,15 +27,21 @@ def rotate(image, angle):
     return rotated, rect
 
 running = True
+clock = pygame.time.Clock()
+
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: running = False
+        if event.type == pygame.QUIT:
+            running = False
 
     now = datetime.datetime.now()
-    # -90 градус - Миккидің қолы басында 3-ке қарап тұрса, 12-ге келтіру үшін
-    sec_angle = -(now.second * 6) + 90 
-    min_angle = -(now.minute * 6) + 90
+    
+    
+    sec_angle = -(now.second * 6)
+    min_angle = -(now.minute * 6)
 
+   
+    screen.fill((255, 255, 255))
     screen.blit(bg, (0, 0))
     
     img_m, rect_m = rotate(hand, min_angle)
@@ -33,5 +51,6 @@ while running:
     screen.blit(img_s, rect_s)
 
     pygame.display.flip()
-    pygame.time.Clock().tick(1)
+    clock.tick(60)
+
 pygame.quit()
